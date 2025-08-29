21819bed719d834a11e787f12273e9358645ce79
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-08-29 19:09:43 +0530

    latest

---
+++ 01-vpc/variables.tf
@@ -0,0 +1,39 @@
+variable "project" {
+  type = string
+  default = "expense"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map
+  default = {
+    Name = "expense"
+    environment = "dev"
+    Terraform = "true"
+  }
+}
+
+variable "public_subnet" {
+  type = list
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet" {
+  type = list
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "database_subnet" {
+  type = list
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "is_peering_required" {
+  type = bool
+  default = true
+}
+
