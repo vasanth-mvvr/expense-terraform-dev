2bc7121c00a761b476f61daa0496c53ec0b64fe1
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-02 10:44:09 +0530

    latest

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -14,7 +14,7 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project}/${var.environment}/private_subnet_ids"
   type = "StringList"
-  value = join(",",data.aws_ssm_parameter.private_subnet_ids)
+  value = join(",",module.vpc.private_subnet_ids)
 }
 
 resource "aws_ssm_parameter" "db_subnet_group_name" {
